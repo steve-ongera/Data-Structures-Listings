9f1c4e3bc9e7c419c6ea8182580fa4e6ca7d7c9b
steve <gadafisteve001@gmail.com>
2025-09-29 17:40:29 +0300

    first commit

---
+++ simplel_list/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a list of strings
+        List<string> fruits = new List<string>();
+
+        // Add items to the list
+        fruits.Add("Apple");
+        fruits.Add("Banana");
+        fruits.Add("Mango");
+
+        // Display items
+        Console.WriteLine("Fruits in the list:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        // Remove an item
+        fruits.Remove("Banana");
+
+        // Access by index
+        Console.WriteLine("\nFirst fruit: " + fruits[0]);
+
+        // Count items
+        Console.WriteLine("Total fruits: " + fruits.Count);
+    }
+}
